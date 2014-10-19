63906e6fcfedf076a831d61b88cb4cb680b4babe
harry2469 <kriswelsh@gmail.com>
2014-10-19 17:41:28 +0100

    comic-rename now retains original extention

--- comic-rename.cmd
+++ comic-rename.cmd
@@ -1,5 +1,5 @@
 ECHO OFF
-REM - -WARNING- - this command will also change all files in the selected folder to a .png - use cautiously.
+REM The files must first be named 1, 2, 3, etc. as the number is taken from the file name
 REM %1 is the name of the folder you want to batch rename in
 REM %2 is the prefix you want to give each of the files
 
@@ -16,7 +16,7 @@ IF "%2"=="" (
 
 IF EXIST %folderName% (
   FOR /F "usebackq" %%f IN (`dir /b %folderName%`) DO (
-    RENAME %folderName%\%%f "%fileName% (%%~nf).png"
+    RENAME %folderName%\%%f "%fileName% (%%~nf)%%~xf"
   )
 ) ELSE (
   SET /P display_user_message="folderName cannot be found in root"
