280be754427570180fa188d57bef14a47096a593
Kristian Welsh <kriswelsh@gmail.com>
2015-03-20 14:47:09 +0000

    Fixed error in init.cmd
    init.cmd was calling setup-ssh.bat instead of the new setup-ssh.cmd

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -1,3 +1,3 @@
 @ echo off
 REM need to extract the first two lines of the output from ssh-agent and slap a set before them then run that as a command, then run ssh-add and we should be golden
-echo setup-ssh.bat has yet to be implemented
+echo setup-ssh.cmd has yet to be implemented
