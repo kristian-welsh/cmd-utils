88a7a7bd09e914c2a601bbdedcad69be815f538a
Kristian Welsh <kriswelsh@gmail.com>
2015-03-22 01:57:44 +0000

    Made progress on setup-ssh
    setup-ssh now extracts the first line of ssh-agent and assigns the proper
    variable, extracting the second line is more dificult as it involves
    looping through the file's lines and counting them to split the variables.

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -1,3 +1,11 @@
 @ echo off
-REM need to extract the first two lines of the output from ssh-agent and slap a set before them then run that as a command, then run ssh-add and we should be golden
-echo setup-ssh.cmd has yet to be implemented
+REM need to extract the first two lines of the output from ssh-agent and slap a set before them then run that as a command minus the end bit), then run ssh-add and we should be golden
+echo setup-ssh.cmd is incomplete
+
+ssh-agent > ssh-agent-output.tmp
+set /p output_line1=<ssh-agent-output.tmp
+del ssh-agent-output.tmp
+
+REM use ssh-agent-output to set the apropriate values
+set %output_line1:~0,-23%
+set SSH_AUTH_SOCK
