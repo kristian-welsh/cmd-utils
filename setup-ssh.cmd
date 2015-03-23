74960df6d1e30186c6927b2ee8917e7fc918af4d
Kristian Welsh <kriswelsh@gmail.com>
2015-03-23 12:22:35 +0000

    Changed destination folder for setup-ssh temp file
    setup-ssh should now work equaly well from any folder
    regardless of whether cmd has access rights to the
    current dir.
    
    Next step is to use the newly set variables to start
    an ssh session using ssh-add

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -1,19 +1,16 @@
 @ echo off
 rem Extract first two lines of output from ssh-agent to set output as a variable (minus end part). Then run ssh-add on id_rsa
-
-rem probably should change the temp file path to be %~dp0
 echo ssh-setup is incomplete
-
 setlocal EnableDelayedExpansion
 
-ssh-agent > ssh-agent-output.tmp
+ssh-agent > "%~dp0ssh-agent-output.tmp"
   rem extract each line of output into it's own variable
   set /a num=0
-  for /f "tokens=*" %%a in (ssh-agent-output.tmp) do (
+  for /f "usebackq tokens=*" %%A in ("%~dp0ssh-agent-output.tmp") do (
     set /a num+=1
-    set output_line!num!=%%a
+    set output_line!num!=%%A
   )
-del ssh-agent-output.tmp
+del "%~dp0ssh-agent-output.tmp"
 
-rem endlocal and call set on the final output lines to complete proccess
+rem remove cruft and call set on the final output lines to complete proccess
 endlocal & set %output_line1:~0,-23% & set %output_line2:~0,-23%
