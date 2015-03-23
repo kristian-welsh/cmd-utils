f6b6152b9df09255aead8dda77f28a62057da687
Kristian Welsh <kriswelsh@gmail.com>
2015-03-23 01:36:02 +0000

    Extended setup-ssh to set second variable as well.
    I still need to change the path for the temp output file.
    It currently outputs the file to the current path, i need
    to change it so that it outputs to the directory of the cmd file.
    
    I also still need to add the rest of the setup process,
    like ssh-add and prompting for a pass phrase.

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -1,11 +1,19 @@
 @ echo off
-REM need to extract the first two lines of the output from ssh-agent and slap a set before them then run that as a command minus the end bit), then run ssh-add and we should be golden
-echo setup-ssh.cmd is incomplete
+rem Extract first two lines of output from ssh-agent to set output as a variable (minus end part). Then run ssh-add on %~%/.ssh/id_rsa
+
+rem probably should change the temp file path to be %~dp0
+echo ssh-setup is incomplete
+
+setlocal EnableDelayedExpansion
 
 ssh-agent > ssh-agent-output.tmp
-set /p output_line1=<ssh-agent-output.tmp
+  rem extract each line of output into it's own variable
+  set /a num=0
+  for /f "tokens=*" %%a in (ssh-agent-output.tmp) do (
+    set /a num+=1
+    set output_line!num!=%%a
+  )
 del ssh-agent-output.tmp
 
-REM use ssh-agent-output to set the apropriate values
-set %output_line1:~0,-23%
-set SSH_AUTH_SOCK
+rem endlocal and call set on the final output lines to complete proccess
+endlocal & set %output_line1:~0,-23% & set %output_line2:~0,-23%
