998fa5ed3e306f631f88c13e1535125caaeab685
Kristian Welsh <kriswelsh@gmail.com>
2015-04-16 00:49:54 +0100

    Improved comments.

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -1,10 +1,11 @@
 @ echo off
-rem count number of active ssh-agent procceses
+
+rem count active ssh-agent procceses
 tasklist /fi "imagename eq ssh-agent.exe" | find /C "ssh-agent.exe" > "%~dp0ssh-agent-task-count.tmp"
   set /p active_ssh_agents=<"%~dp0ssh-agent-task-count.tmp"
 del "%~dp0ssh-agent-task-count.tmp"
 
-rem if there isn't an active ssh session, start a new one.
+rem if ssh isn't running, start ssh.
 if %active_ssh_agents% lss 1 (
   echo starting new ssh session.
   call start-ssh.cmd
--- start-ssh.cmd
+++ start-ssh.cmd
@@ -1,24 +1,29 @@
-rem start an ssh session
+rem Start an ssh session.
+rem Set needed variables from ssh-agent output, then run ssh-add.
 @ echo off
 
-rem Extract output from ssh-agent and set variables from output, then run ssh-add
-
+rem Delayed expansion needed to seperate output lines.
 setlocal EnableDelayedExpansion
-
+  
+  rem Save ssh-agent information to file for recovery.
   ssh-agent > "%~dp0ssh-agent-output.tmp"
-    rem extract each line of output into it's own variable
+
+    rem Extract each line of output into it's own variable.
     set /a num=0
     for /f "usebackq tokens=*" %%A in ("%~dp0ssh-agent-output.tmp") do (
       set /a num+=1
       set ssh_agent_output_line!num!=%%A
     )
+
   del "%~dp0ssh-agent-output.tmp"
   
-  rem extract data from output
+  rem Extract data from output lines.
   set SSH_AUTH_SOCK=%ssh_agent_output_line1:~14,32%
   set SSH_AGENT_PID=%ssh_agent_output_line2:~14,4%  
+  
 endlocal&(
 set SSH_AUTH_SOCK=%SSH_AUTH_SOCK%
 set SSH_AGENT_PID=%SSH_AGENT_PID%)
 
+rem ssh-add needs to happen outside of local space.
 ssh-add "%userprofile%\.ssh\id_rsa"
